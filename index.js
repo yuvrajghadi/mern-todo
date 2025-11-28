79bcd513f8c849c00e9286aec45d9a8cb3591e7d
yuvrajghadi <ghadiyuvraj08@gmail.com>
2025-11-28 19:39:11 +0530

    created ejs pages and created express server

---
+++ index.js
@@ -0,0 +1,46 @@
+import express from "express";
+import path from "path";
+import { MongoClient } from "mongodb";
+
+const publicPath = path.resolve("public");
+const app = express();
+app.use(express.static(publicPath));
+app.use(express.urlencoded({extended:false}))
+
+const dbName = 'nodejs'
+const collection = 'todo'
+const url = 'mongodb://localhost:27017'
+const client =new MongoClient(url)
+
+const connectDB = async () => {
+  await client.connect();           // properly connect
+  return client.db(dbName);         // return the database
+};
+
+
+
+app.set("view engine", "ejs");
+app.get("/", (req, res) => {
+  res.render("list");
+});
+
+app.get("/add", (req, res) => {
+  res.render("add");
+});
+
+app.get("/update", (req, res) => {
+  res.render("update");
+});
+app.post("/add", async(req, res) => {
+      console.log(req.body)
+
+    const db = await connectDB()
+
+    const connection = db.collection(collection)
+    const result = connection.insertOne(req.body)
+
+  res.redirect("/");
+});
+
+
+app.listen(3200);
